4443209f48505a180d94fbf0f490e83d9d6c6f55
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2018-03-07 12:24:05 -0500

    Add front index module

---
+++ app/modules/frontIndex/views/index.php
@@ -0,0 +1,12 @@
+{% include App\Modules\FrontIndex\FrontIndexController:header %}
+
+<h1>Example - Skeleton App</h1>
+
+<p>
+    Sample app created with <?php echo $this->dictionary['name']; ?>.<br/>
+    <br/>
+    More info at:<br/>
+    <a href="https://github.com/riveraframework/example-skeleton">https://github.com/riveraframework/example-skeleton</a>
+</p>
+
+{% include App\Modules\FrontIndex\FrontIndexController:footer %}
