dde6ff3f352ccbe90e5cdac749b2e919de7fbf21
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2018-03-07 12:23:11 -0500

    Add public handler and app bootstrap

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+	'app' => [
+		'name' => 'Skeleton App',
+		'domain' => 'rf-skeleton.local',
+		'url' => 'http://rf-skeleton.local',
+		'available-domains' => 'rf-skeleton.local',
+		'available-languages' => 'en',
+	],
+
+];
